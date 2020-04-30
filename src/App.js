a93b47f2e9e6beea70e18851ce4880a29a5b1f3a
Emin <eminaydin93@yahoo.com>
2020-04-30 16:53:17 +0200

    Error functionality added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React, { useState } from 'react';
 import './App.css';
-import { findAllInRenderedTree } from 'react-dom/test-utils';
 
 function App() {
   const [word, setWord] = useState([]);
   const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(true);
+  const [showMessage, setShowMessage] = useState();
 
   function submit(e) {
     e.preventDefault();
-    if (userInput === "") {
-      window.alert("Nope sorry")
-    } else {
-      word.push(userInput);
-      setWord(word);
-      console.log(word);
-      setUserInput("");
-    }
+
+    if (word.length <= 3) {
+      setError(true)
+      setShowMessage(false)
+    } else { setError(false) }
+    word.push(userInput);
+    setWord(word);
+    console.log(word);
+    console.log(error);
+    setUserInput("");
   }
 
-
+  function display(e) {
+    e.preventDefault();
+    if (word.length < 3) {
+      setShowMessage(true)
+      console.log(showMessage);
+    } else {
+      setShowMessage(false);
+      console.log(showMessage)
+    }
+  }
   return (
     <div className="App">
       <form>
-        <input type="text" id="name" name="name" required value={userInput} onChange={e => setUserInput(e.target.value)}
-        />
+        <input type="text" id="name" name="name" value={userInput} onChange={e => setUserInput(e.target.value)} />
+        {showMessage ? (<p>sorry some more</p>) :
+          ("")}
         <button onClick={submit} type="submit">Add</button>
-        <button>Show</button>
+        <button onClick={display}>Show</button>
+
       </form>
     </div>
   );
