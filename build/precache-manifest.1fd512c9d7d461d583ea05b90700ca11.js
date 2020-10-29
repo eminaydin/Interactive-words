f97903dc19217d1a79fac00aa82657b1b335b741
eminaydin <eminaydin93@yahoo.com>
2020-10-29 22:18:57 +0100

    Deploy site

---
+++ build/precache-manifest.1fd512c9d7d461d583ea05b90700ca11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592c57d344c19fab9f98480015c6f01e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f220d70867d1e52045a8",
+    "url": "/static/css/main.fca0ee9d.chunk.css"
+  },
+  {
+    "revision": "72c17aacbcf740a1c312",
+    "url": "/static/js/2.fb12cd8e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.fb12cd8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f220d70867d1e52045a8",
+    "url": "/static/js/main.578202a9.chunk.js"
+  },
+  {
+    "revision": "ea7cee67bfe696615ef9",
+    "url": "/static/js/runtime-main.4c6c3c1c.js"
+  }
+]);
